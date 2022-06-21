c7e4a76784d626567c7a3936e0d5f3f456c1675e
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-21 13:17:52 -0700

    Added nav to default layout.

--- views/defalt.jsx
+++ views/defalt.jsx
@@ -1,12 +1,28 @@
 const React = require('react');
-
+// Default Html layout with nav
 const Def = (html) => {
     return(
+        // Start of html layout
         <html>
             <head>
                 <title>Title</title>
             </head>
             <body>
+                {/* Start of nav */}
+                <nav>
+                    {/* Nav container */}
+                    <div className="nav-container">
+                        {/* Nav logo */}
+                        <div className="logo">logo</div>
+                        {/* Nav links */}
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                {/* End of nav */}
                 {html.children}
             </body>
         </html>
