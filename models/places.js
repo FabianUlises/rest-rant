d727684c4ad36f40d943063442c4c82b90daec8c
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-22 18:11:55 -0700

    moved model data to model folder

---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'http://placekitten.com/200/300'
+}, {
+    name: 'Cidubg Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'http://placekitten.com/200/300'
+}
+];
--- routes/places.js
+++ routes/places.js
@@ -1,26 +1,23 @@
 const router = require('express').Router();
+const places = require('../models/places');
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/200/300'
-    }, {
-        name: 'Cidubg Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/200/300'
-    }
-];
     res.status(206).render('places/index', { places });
 });
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.status(206).send('Create a new place');
-});
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body);
+    res.redirect('/places');
+  });
 router.get('/new', (req, res) => {
     res.status(206).render('places/new');
 });
