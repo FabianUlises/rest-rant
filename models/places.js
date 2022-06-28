e25961052e32a477fd24a9776a9b774ab9725a13
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-28 14:22:50 -0700

    Added custom method to places model.

--- models/places.js
+++ models/places.js
@@ -8,4 +8,7 @@ const placeSchema = new mongoose.Schema({
   state: { type: String, default: 'USA' },
   founded: Number
 })
+placeSchema.methods.showEstablished = function() {
+  return`${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}`
+}
 module.exports = mongoose.model('Place', placeSchema)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,14 +7,13 @@ const show = (data) => {
             <main>
                 <div className="place-image">
                     <img src={data.place.pic} alt={data.place.name} />
+                    <h3>Located in {data.place.city}, {data.place.state}</h3>
                 </div>
                 <div className="place-desc">
-                    <h2>{data.place.name}</h2>
-                    <h4>{data.place.cuisines}</h4>
-                    <div className="place-location">
-                        <p>{data.place.city}, {data.place.state}</p>
-                    </div>
+                    <h3>{data.place.showEstablished()}</h3>
+                    <h4>Serving {data.place.cuisines}</h4>
                 </div>
+
                 <div className="place-btn-container">
                     <a className='btn btn-warning' href={`/places/${data.id}/edit`}>Edit</a>
                     <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
