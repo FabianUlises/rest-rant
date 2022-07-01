ff4719f868213df5bd64f3bebcde370f8495df5c
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-30 17:50:13 -0700

    Able to add comment to place

--- routes/places.js
+++ routes/places.js
@@ -62,9 +62,26 @@ router.get('/:id/edit', (req, res) => {
   res.send('GET edit form stub')
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  // req.body.rant = req.body.rant ? true : false
+  // res.send('GET /places/:id/rant stub')
+  db.Place.findById(req.params.id)
+    .then(place => {
+      db.Comment.create(req.body)
+        .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+        })
+        .catch (err => {
+          res.render('error404');
+        })
+    })
 })
+
 
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
