43e628a8ed00a5fc4350d9651ca4330394affd75
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-11 12:59:52 -0700

    Converted all route handlers to with try catch

--- controllers/place_controller.js
+++ controllers/place_controller.js
@@ -1,95 +1,84 @@
-const db = require('../models')
+const db = require('../models');
 exports.getAllPlaces = async (req, res) => {
     try {
         const places = await db.Place.find();
-        res.render('places/index', { places })
+        res.render('places/index', { places });
     } catch (err) {
-        res.render('error404')
+        res.render('error404');
     }
-}
+};
 exports.addPlace = async (req, res) => {
     try {
-        if(req.body.pic === '') {req.body.pic = undefined}
-        if(req.body.city === '') {req.body.city = undefined}
-        if(req.body.state === '') {req.body.state = undefined}
+        if(req.body.pic === '') {req.body.pic = undefined};
+        if(req.body.city === '') {req.body.city = undefined};
+        if(req.body.state === '') {req.body.state = undefined};
         await db.Place.create(req.body);
-            res.redirect('/places')
+            res.redirect('/places');
     } catch (err) {
         if(err && err.name == 'ValidationError') {
-            let message = 'Validation Error'
+            let message = 'Validation Error';
             for(let el in err.errors) {
             message += `${el} was ${err.errors[el].value}`
             message += `${err.errors[el].message}`
-            }
+            };
             console.log('Validation error message', message)
             res.render('places/new', { message })
         } else {
-            res.render('error404')
+            res.render('error404');
         }
     }
-}
+};
 exports.getPlace = async (req, res) => {
     try {
-        const place = await db.Place.findById(req.params.id).populate('comments')
-        res.render('places/show', { place })
+        const place = await db.Place.findById(req.params.id).populate('comments');
+        res.render('places/show', { place });
     } catch (err) {
-        res.render('error404')
+        res.render('error404');
     }
-}
-exports.updatePlace = (req, res) => {
-    db.Place.findByIdAndUpdate(req.params.id, req.body)
-    .then(() => {
-        res.redirect(`/places/${req.params.id}`)
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
-}
-exports.deletePlace = (req, res) => {
-    db.Place.findByIdAndDelete(req.params.id)
-    .then(place => {
-        res.redirect('/places')
-    })
-    .catch(err => {
-        // console.log('err', err)
-        res.render('error404')
-    })
-}
-exports.updatePlaceView = (req, res) => {
-    db.Place.findById(req.params.id)
-    .then(place => {
-        res.render('places/edit', { place })
-    })
-    .catch(err => {
-        res.render('error404')
-    })
-}
+};
+exports.updatePlace = async (req, res) => {
+    try {
+        await db.Place.findByIdAndUpdate(req.params.id, req.body);
+        res.redirect(`/places/${req.params.id}`);
+    } catch(err) {
+        res.render('error404');
+    }
+};
+exports.deletePlace = async(req, res) => {
+    try {
+        await db.Place.findByIdAndDelete(req.params.id);
+        res.redirect('/places');
+    } catch (err) {
+        res.render('error404');
+    }
+};
+exports.updatePlaceView = async (req, res) => {
+    try {
+        const place = await db.Place.findById(req.params.id);
+        res.render('places/edit', { place });
+    } catch (err) {
+        res.render('error404');
+    }
+};
 
-exports.addComment = (req, res) => {
-    req.body.rant = req.body.rant ? true : false
-    // res.send('GET /places/:id/rant stub')
-    db.Place.findById(req.params.id)
-        .then(place => {
-        db.Comment.create(req.body)
-            .then(comment => {
-            place.comments.push(comment.id)
-            place.save()
-                .then(() => {
-                    res.redirect(`/places/${req.params.id}`)
-                })
-            })
-            .catch (err => {
-                res.render('error404');
-            })
-        })
-}
-exports.deleteComment = (req, res) => {
-    db.Comment.findByIdAndDelete(req.params.rantId)
-        .then(deleteComment => {
-        res.redirect('/places')
-        })
-        .catch(err => {
-        res.render('error404')
-        })
-}
+exports.addComment = async (req, res) => {
+    try {
+        req.body.rant = req.body.rant ? true : false;
+        const place = await db.Place.findById(req.params.id);
+        const comment = await db.Comment.create(req.body);
+        place.comments.push(comment.id);
+        await place.save();
+        res.redirect(`/places/${req.params.id}`);
+    } catch (err) {
+        res.render('error404');
+    }
+};
+exports.deleteComment = async (req, res) => {
+    try {
+        await db.Comment.findByIdAndDelete(req.params.rantId);
+        res.redirect('/places');
+
+    } catch (err) {
+        res.render('error404');
+    }
+};
