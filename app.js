de47f8c90f49b138206418605736f4f5ae0d32f9
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-11 13:35:40 -0700

    heroku

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-// const methodOverride = require('method-override');
+const methodOverride = require('method-override');
 //  Express settings
 app.set('views', __dirname + '/views')
 app.engine('jsx', require('express-react-views').createEngine())
 app.set('view engine', 'jsx');
 app.use(express.static('public'))
-// app.use(express.urlencoded({ extended: true }))
-// app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
 // // Controllers and routes
-// app.use('/places', require('./routes/places'));
+app.use('/places', require('/routes/places'));
 // Root route / Home
 app.get('/', (req, res) => {
     res.status(200).render('Home');
