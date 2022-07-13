d3660a16afac5c3eb51626484ec0cac3128a5544
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-13 07:51:20 -0700

    Fully workign CRUD project.

--- app.js
+++ app.js
@@ -2,22 +2,24 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 //  Express settings
 app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine())
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
-const db = require('./models')
+// Controllers and routes
+app.use('/places', require('./routes/places'));
 // Root route / Home
-app.get('/', (req, res) => {
-    res.status(200).render('Home');
+// app.get('/', (req, res) => {
+//     res.status(200).render('Home');
+// });
+// Catch all error
+app.get('*', (req, res) => {    
+    res.status(404).render('error404');
 });
-
-app.get('/places', (req, res) => {
-    res.status(200).render('places/Index')
-})
-
+// Server on
 app.listen(process.env.PORT);
